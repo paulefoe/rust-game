f2fdc73f82315418d06615ef3709d917c1b5e6fa
Pavel Filatov <pavel-star@spoton.com>
2021-05-02 09:15:17 +0300

    initial commit

---
+++ src/components.rs
@@ -0,0 +1,30 @@
+use rltk::{RGB};
+use specs::prelude::*;
+use specs_derive::Component;
+
+#[derive(Component, Debug)]
+pub struct Viewshed {
+    pub visible_tiles: Vec<rltk::Point>,
+    pub range: i32,
+    pub dirty: bool
+}
+
+#[derive(Component, Debug)]
+pub struct Monster {}
+
+#[derive(Component, Copy, Clone, Debug)]
+pub struct Position {
+    pub x: i32,
+    pub y: i32,
+}
+
+
+#[derive(Component)]
+pub struct Renderable {
+    pub glyph: rltk::FontCharType,
+    pub fg: RGB,
+    pub bg: RGB,
+}
+ 
+#[derive(Component, Debug)]
+pub struct Player {}
